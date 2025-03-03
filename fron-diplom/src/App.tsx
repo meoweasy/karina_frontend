2d100158c094bd076cb31dcb4c09718f206315f4
meoww <meowweasy@mail.ru>
2025-03-03 16:03:44 +0400

    1

--- fron-diplom/src/App.tsx
+++ fron-diplom/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <AuthorPage
         header_image="https://avatars.mds.yandex.net/i?id=4cd0cb13f46031bc22e9be6bcd199074_l-7761368-images-thumbs&n=13"
-        fio_author="Гордеева Анна Евгеньевна"
+        fio_author="Иванов Иван Иванович"
         desc_author="Краткое описание чем занимается автор, что ему интересно и с чем работает. Что-то еще можно сюда же написать. Необходимо придумать"
         image_author="https://cdn.forbes.ru/forbes-static/c/1824x1026/new/2023/10/1-GettyImages-755650739-6529480935f2d.jpg"
       />
